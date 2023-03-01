ba350aa857bbaf51d8d4b303271eaa70c9849b55
kwYoohae <gocksdl7979@gmail.com>
2023-03-01 15:16:38 +0900

    [프로젝트 환경설정] JPA와 DB 설정, 동작확인

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save (Member member) {
+        em.persist(member);
+        return member.getId(); // 사이드 이펙트를 위한 방지
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+        
+        //when
+        final Long savedId = memberRepository.save(member);
+        final Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
