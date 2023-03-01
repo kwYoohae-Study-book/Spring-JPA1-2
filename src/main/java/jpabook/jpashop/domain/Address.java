0ea24c1247e4014d3cea16505a845f739e07b3c2
kwYoohae <gocksdl7979@gmail.com>
2023-03-01 20:10:51 +0900

    [도메인 분석 설계] 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,18 @@ import lombok.Getter;
 @Embeddable
 @Getter
 public class Address {
-
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+
+    }
+
+    public Address(final String city, final String street, final String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
 }
