59b0ba494f102f078a026cceab3c371490f375ea
kwYoohae <gocksdl7979@gmail.com>
2023-03-01 19:51:45 +0900

    [도메인 분석 설계] 엔티티 클래스 개발1

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
