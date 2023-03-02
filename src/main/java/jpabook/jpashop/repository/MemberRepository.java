c32af03bb078e67e6cc2f62c0e306dd43eb6f5e8
kwYoohae <gocksdl7979@gmail.com>
2023-03-02 10:18:15 +0900

    [회원 도메인 개발] 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,15 +2,16 @@ package jpabook.jpashop.repository;
 
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // persistenceContext가 없어도 Data JPA 때문에 생성자 주입으로 사용 가능
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
