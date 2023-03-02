70ba0ac570e5d44cb32cf8f7dcba9d0bb7bb7177
kwYoohae <gocksdl7979@gmail.com>
2023-03-02 17:15:50 +0900

    [주문 도메인 개발] 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -12,10 +12,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class OrderItem {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
@@ -29,4 +31,26 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //== 생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //== 비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); // 재고수량 복구
+    }
+
+    //== 조회 로직 ==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
