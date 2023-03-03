d8f1fb8f49e6f60484313aadaca9fdd6f7c17e03
kwYoohae <gocksdl7979@gmail.com>
2023-03-03 12:31:59 +0900

    [웹 계층 개발] 회원 목록 조회

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,22 @@ public class JpashopApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JpashopApplication.class, args);
-  }
 
+  }
+  public int[] solution(int[] emergency) {
+    int[] answer = new int[emergency.length];
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    int[] copy = Arrays.copyOf(emergency, emergency.length);
+    Arrays.sort(copy);
+
+    for (int i = 1; i <= copy.length; i++) {
+      map.put(copy[i - 1] , i);
+    }
+
+    for (int i = 0; i < emergency.length; i++) {
+      answer[i] = map.get(emergency[i]);
+    }
+    return answer;
+  }
 }
